657423e2b6e47741568999b5ace5ad0a145947c7
yunitanurjanah <yunitanurjanah7@gmail.com>
2019-07-26 13:26:36 +0700

    update tampilan list stack (#24)

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,7 @@
 
 Route::get('/tiketux/rancherprojects/api/list', 'Tiketux\RancherProjects\Api\RancherProjectsApi@listAll');
 Route::get('/tiketux/rancher/stack/api/liststack', 'Tiketux\RancherProjects\Api\RancherStackApi@listStackAll');
+Route::post('/tiketux/rancher/stack/api/detailstack', 'Tiketux\RancherProjects\Api\RancherStackApi@detailStackOnline');
 Route::get('/tiketux/rancher/stack/api/liststackdb', 'Tiketux\RancherProjects\Api\RancherStackApi@listStackDB');
 Route::post('/tiketux/rancher/stack/api/addstackdb', 'Tiketux\RancherProjects\Api\RancherStackApi@addStacktoDB');
 Route::post('/tiketux/rancher/stack/api/cekstackdb', 'Tiketux\RancherProjects\Api\RancherStackApi@cekStackDB');
@@ -9,3 +10,4 @@ Route::post('/tiketux/rancher/stack/api/deletestackdb', 'Tiketux\RancherProjects
 Route::post('/tiketux/rancher/stack/api/listserviceonstack', 'Tiketux\RancherProjects\Api\RancherStackApi@listServiceOnStack');
 Route::post('/tiketux/rancher/stack/api/addservicetodb', 'Tiketux\RancherProjects\Api\RancherStackApi@addServicetoDB');
 Route::post('/tiketux/rancher/stack/api/cekserviceindb', 'Tiketux\RancherProjects\Api\RancherStackApi@cekServiceOnDB');
+Route::post('/tiketux/rancher/stack/api/detailservicestackdb', 'Tiketux\RancherProjects\Api\RancherStackApi@detailServiceStackOnDB');
